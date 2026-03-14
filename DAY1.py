1f06f7cba4ddc0295edc5077e986ae39dfd19f26
Ayush Tomar <ayushtomar@Ayushs-MacBook-Air.local>
2026-03-14 19:20:37 +0530

    Day 1: My first Python loop

---
+++ DAY1.py
@@ -0,0 +1,31 @@
+#Task 1
+#
+#Ask user for two numbers and print their sum.
+"""num1=int(input("enter your first number "))
+num2=int(input("enter your second number "))
+add = num1+num2"""
+#print(f"Here is the sum of these two numbers {add}")
+
+#Task 2
+#Ask user marks.
+
+#If marks ≥ 50 → Pass
+#Else → Fail
+
+"""Marks= int(input("please enter your marks "))
+if Marks >= 50:
+    print("You are passed!")
+else:
+
+    print("You are failed!")"""
+
+#Task 3
+#Task 3Print numbers 1 to 10 using a loop
+"""for i in range (1,11):
+    print(i)
+"""
+#Task 4
+#function
+def a():
+    print("This is a function")
+a()
